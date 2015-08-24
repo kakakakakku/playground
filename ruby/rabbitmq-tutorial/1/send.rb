036ec5cccbba672189443d5b82306c2e91d7009a
kakakakakku <y.yoshida22@gmail.com>
2015-08-24 22:51:56 +0900

    Added rabbitmq-tutorial base code

---
+++ ruby/rabbitmq-tutorial/1/send.rb
@@ -0,0 +1,12 @@
+require "bunny"
+
+conn = Bunny.new(:automatically_recover => false)
+conn.start
+
+ch = conn.create_channel
+q = ch.queue("hello")
+
+ch.default_exchange.publish("Hello World!", :routing_key => q.name)
+puts " [x] Sent 'Hello World!'"
+
+conn.close
---
+++ ruby/rabbitmq-tutorial/2/new_task.rb
@@ -0,0 +1,14 @@
+require "bunny"
+
+conn = Bunny.new(:automatically_recover => false)
+conn.start
+
+ch = conn.create_channel
+q = ch.queue("task_queue", :durable => true)
+
+msg = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
+
+q.publish(msg, :persistent => true)
+puts " [x] Sent #{msg}"
+
+conn.close
