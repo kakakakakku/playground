b65692315747d93a1cae77146b6dacbfab96be88
kakakakakku <y.yoshida22@gmail.com>
2015-08-30 18:16:47 +0900

    Added parrot-bot lambda function

--- aws-lambda/parrot-bot/index.js
+++ aws-lambda/parrot-bot/index.js
@@ -1,7 +1,10 @@
 exports.handler = function(event, context) {
+    message = event.split('&')[9].split('=')[1];
+    returnMessage = message.replace(/parrotbot\+/g,'');
+
     var exec = require('child_process').exec;
     var hookUrl = 'https://hooks.slack.com/services/xxx'
-    var cmd = 'curl -X POST --data-urlencode \'payload={"text": "Hello Slack from AWS Lambda"}\' ' + hookUrl
+    var cmd = 'curl -X POST --data-urlencode \'payload={"text": "' + returnMessage + '"}\' ' + hookUrl
     exec(cmd, function(error, stdout, stderr) {
         context.succeed(stdout);
     });
