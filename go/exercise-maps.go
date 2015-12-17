315b210c43b6373c2495dc6391f3cae6a73d66fc
kakakakakku <y.yoshida22@gmail.com>
2015-12-17 17:32:45 +0900

    Updated exercise-maps.go

--- go/exercise-maps.go
+++ go/exercise-maps.go
@@ -16,6 +16,18 @@ func WordCount(s string) map[string]int {
 	return wordMap
 }
 
+// PASS
+//  f("I am learning Go!") =
+//   map[string]int{"I":1, "am":1, "learning":1, "Go!":1}
+// PASS
+//  f("The quick brown fox jumped over the lazy dog.") =
+//   map[string]int{"The":1, "quick":1, "jumped":1, "over":1, "the":1, "lazy":1, "dog.":1, "brown":1, "fox":1}
+// PASS
+//  f("I ate a donut. Then I ate another donut.") =
+//   map[string]int{"ate":2, "a":1, "donut.":2, "Then":1, "another":1, "I":2}
+// PASS
+//  f("A man a plan a canal panama.") =
+//   map[string]int{"panama.":1, "A":1, "man":1, "a":2, "plan":1, "canal":1}
 func main() {
 	wc.Test(WordCount)
 }
