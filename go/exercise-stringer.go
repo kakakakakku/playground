12f0ecfaca1756251bbe28fae91cd253eca10def
kakakakakku <y.yoshida22@gmail.com>
2015-12-18 16:42:13 +0900

    Added exercise-stringer.go

---
+++ go/exercise-stringer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
+
+// googleDNS: 8.8.8.8
+// loopback: 127.0.0.1
+func main() {
+	addrs := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for n, a := range addrs {
+		fmt.Printf("%v: %v\n", n, a)
+	}
+}
