922da89fed74b096ab5a17151d8bab6e45975c01
kakakakakku <y.yoshida22@gmail.com>
2016-01-19 16:23:58 +0900

    Refactored gomoku-kai-01.go

--- go/gomoku-kai-02/gomoku-kai-01.go
+++ go/gomoku-kai-02/gomoku-kai-01.go
@@ -5,19 +5,36 @@ import (
 	"strings"
 )
 
-func main() {
-	center := 4
+// 中心点を定義する
+var center = 4
 
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+func main() {
+
+	// 上4段
 	for i := 1; i <= 7; i++ {
 		if i%2 == 1 {
-			fmt.Printf("%s%s\n", strings.Repeat(" ", center-i/2), strings.Repeat("*", i))
+			// 奇数
+			outLine(i)
 		}
 	}
 
+	// 下3段
 	for i := 5; i >= 1; i-- {
 		if i%2 == 1 {
-			fmt.Printf("%s%s\n", strings.Repeat(" ", center-i/2), strings.Repeat("*", i))
+			// 奇数
+			outLine(i)
 		}
 	}
+}
 
+func outLine(i int) {
+	// 中心点までの空白の数と出力文字の数を計算して出力する
+	fmt.Printf("%s%s\n", strings.Repeat(" ", center-i/2), strings.Repeat("*", i))
 }
