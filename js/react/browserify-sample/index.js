786b81bbad0d3fb8d3bb7670ad3bd342d51c1e69
kakakakakku <y.yoshida22@gmail.com>
2016-02-12 19:34:34 +0900

    Added browserify require

--- js/react/browserify-sample/index.js
+++ js/react/browserify-sample/index.js
@@ -1,3 +1,8 @@
 var React = require('react');
 
 React.render(<h1>Hello World!</h1>, document.body);
+
+// require uniq package by npm
+var uniq = require('uniq');
+var nums = [ 5, 2, 1, 3, 2, 5, 4, 2, 0, 1 ];
+console.log(uniq(nums));
