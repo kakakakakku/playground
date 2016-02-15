c4ca633fe162eee77cbe1ce3045a55429f09af5e
kakakakakku <y.yoshida22@gmail.com>
2016-02-15 16:23:01 +0900

    Added check_initialisms.go

---
+++ go/check_initialisms.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var myHttp should be myHTTP
+	myHTTP := "myHTTP"
+	fmt.Println(myHTTP)
+
+	// var myUrl should be myURL
+	myURL := "myURL"
+	fmt.Println(myURL)
+}
