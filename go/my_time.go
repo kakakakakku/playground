5fdc34ba843382a5c3cfa9e9822b4a6d89ada6e1
kakakakakku <y.yoshida22@gmail.com>
2016-03-27 23:42:01 +0900

    Updated my_time.go

--- go/my_time.go
+++ go/my_time.go
@@ -12,11 +12,12 @@ func main() {
 	n := time.Now()
 
 	// 標準ライブラリでフォーマットする
-	fmt.Println(n)                       // 2016-03-25 19:05:54.785208 +0900 JST
-	fmt.Println(n.Format(time.ANSIC))    // Fri Mar 25 19:05:54 2016
-	fmt.Println(n.Format(time.RubyDate)) // Fri Mar 25 19:05:54 +0900 2016
-	fmt.Println(n.Format(time.RFC3339))  // 2016-03-25T19:05:54+09:00
-	fmt.Println(n.Format("2006-01-02"))  // 2016-03-25
+	fmt.Println(n)                               // 2016-03-25 19:05:54.785208 +0900 JST
+	fmt.Println(n.Format("2006-01-02"))          // 2016-03-25
+	fmt.Println(n.Format("2006-01-02 15:04:05")) // 2016-03-25 19:05:54
+	fmt.Println(n.Format(time.ANSIC))            // Fri Mar 25 19:05:54 2016
+	fmt.Println(n.Format(time.RubyDate))         // Fri Mar 25 19:05:54 +0900 2016
+	fmt.Println(n.Format(time.RFC3339))          // 2016-03-25T19:05:54+09:00
 
 	// timeutil を使ってフォーマットする
 	fmt.Println(timeutil.Strftime(&n, "%Y-%m-%d"))             // 2016-03-25
