937891355d75a25690ae7193af5564b6f3fc707c
kakakakakku <y.yoshida22@gmail.com>
2018-12-31 12:25:50 +0900

    Added retriable.rb

---
+++ ruby/retriable.rb
@@ -0,0 +1,17 @@
+# https://github.com/kamui/retriable
+require 'retriable'
+
+attempt = 0
+
+begin
+  Retriable.retriable(base_interval: 1.0, tries: 10) do
+    now = Time.now
+    puts '[処理開始] ' + now.to_s
+    attempt += 1
+    raise StandardError
+  end
+rescue StandardError
+  puts '----------------------------------------------------------------'
+  puts 'リトライを' + attempt.to_s + '回繰り返しましたが、エラーは解消しませんでした'
+  puts '----------------------------------------------------------------'
+end
