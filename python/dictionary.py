446d95ac44aa41f811f1865b94ac73e6b4cb119d
kakakakakku <y.yoshida22@gmail.com>
2019-01-01 15:22:21 +0900

    Added Python codes

---
+++ python/dictionary.py
@@ -0,0 +1,21 @@
+name = '123456789'
+
+# 123456789
+print(name)
+
+# 1
+print(name[0])
+
+# 2
+print(name[1])
+
+# 2345678
+print(name[1:8])
+
+# 2468
+# slice(start, stop, step)
+print(name[1:8:2])
+
+# 258
+# slice(start, stop, step)
+print(name[1:8:3])
