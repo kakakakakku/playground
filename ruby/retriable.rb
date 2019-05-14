68f18c06ae9b61eac4e4110433c0b3be6e2a5d48
kakakakakku <y.yoshida22@gmail.com>
2019-05-14 22:35:23 +0900

    Updated retriable.rb

--- ruby/retriable.rb
+++ ruby/retriable.rb
@@ -6,11 +6,13 @@ attempt = 0
 begin
   Retriable.retriable(base_interval: 1.0, tries: 9) do
     now = Time.now
-    puts '[処理開始] ' + now.to_s
+    puts ' エラー🔥' if attempt >= 1
+    print '[処理開始] ' + now.to_s
     attempt += 1
     raise StandardError
   end
 rescue StandardError
+  puts ' エラー🔥'
   puts '----------------------------------------------------------------'
   puts 'リトライを' + attempt.to_s + '回繰り返しましたが、エラーは解消しませんでした'
   puts '----------------------------------------------------------------'
